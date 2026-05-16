eeec27cbca673517f51b57f091931d1198f93fb9
zongyang <taozongyang-work@outlook.com>
2026-05-16 16:54:18 +0800

    0.0.1.5 non_blocking_delay

--- core/main.cpp
+++ core/main.cpp
@@ -1,22 +1,16 @@
 #include "main.hpp"
-import indicator;
-
 extern "C" void SystemClock_Config(void);
 
-void NON_BLOCKING_Delay(uint32_t* static_timestamp, uint32_t time, const std::function<void()>& func) {
-  if (HAL_GetTick() - *static_timestamp > time) {
-    *static_timestamp = HAL_GetTick();
-    func();
+import indicator;
+
+template <auto AnonymousTag = [] {}, typename F>
+void NON_BLOCKING_Delay(uint32_t time, F&& func) {
+  static uint32_t timestamp = HAL_GetTick();
+  if (HAL_GetTick() - timestamp >= time) {
+    timestamp = HAL_GetTick();
+    std::forward<F>(func)();
   }
 }
-
-void NON_BLOCKING_Delay(uint32_t* static_timestamp, uint32_t time, const std::function<void(void)>& func);
-// 此宏不可在成员方法中调用，因为静态局部变量不属于独立对象
-#define NON_BLOCKING_DELAY(time, func)                                                                                 \
-  do {                                                                                                                 \
-    static uint32_t timeout_##__COUNTER__ = 0;                                                                         \
-    NON_BLOCKING_Delay(&timeout_##__COUNTER__, time, func);                                                            \
-  } while (0)
 
 int main() {
   HAL_Init();
@@ -30,6 +24,6 @@ int main() {
   MX_SPI3_Init();
   puts("Hello LLVM-libc via UART!");
   while (true) {
-    NON_BLOCKING_DELAY(500, Indicator);
+    NON_BLOCKING_Delay(500, Indicator);
   }
 }
